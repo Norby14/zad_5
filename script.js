fd5e878c11b0dcdc326af46da1ec7947a67dd675
Norby141 <100698239+Norby14@users.noreply.github.com>
2022-05-10 12:56:08 +0000

    commit 2

--- script.js
+++ script.js
@@ -15,7 +15,7 @@
   })
 
   cw1.addEventListener("click", function () {
-		answer.innerHTML = "Ladowanie..."
+		answer.innerHTML = "Loading..."
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
 			.then(function(data){
